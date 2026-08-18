3a9b2632756b6cb5b8549b2c42e042c41c4d5bf2
Silsuerenller <silelusar@gmail.com>
2026-08-17 23:51:46 -0400

    Update servicios.php

--- Semana-2/servicios.php
+++ Semana-2/servicios.php
@@ -1,137 +1,59 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>Primera pagina</title>
+        <title>primera pagina</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <!-- Navbar -->
+        <!--navbar-->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <div class="container-fluid"> 
-                <a class="navbar-brand" href="index.php"><i class="material-icons">star_border</i></a>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><i class="fa fa-star-half-o" style="font-size:48px;color:white"></i></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="marcaderopa.php" role="button" data-bs-toggle="dropdown">Marca de Ropa</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Quienes Somos</a></li>
-                                <li><a class="dropdown-item" href="#">Nuestro Equipo</a></li>
-                                <li><a class="dropdown-item" href="#">Mision</a></li>
-                            </ul>
-                        </li>                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="productos.php">Productos</a>
+                            <a class="nav-link dropdown-toggle" href="marcaderopa.php" role="button" data-bs-toggle="dropdown">Prendas</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="marcaderopa.php">Poleras</a></li>
+                            <li><a class="dropdown-item" href="marcaderopa.php">Polerones</a></li>
+                            <li><a class="dropdown-item" href="marcaderopa.php">Pantalones</a></li>
+                            <li><a class="dropdown-item" href="marcaderopa.php">Buzos</a></li>
+                            <li><a class="dropdown-item" href="productos.php">Accesorios</a></li>
+                        </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="servicios.php">Servicios</a>
+                            <a class="nav-link" href="productos.php">productos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contacto.php">Contacto</a>
+                            <a class="nav-link" href="servicios.php">servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contacto.php">contacto</a>
                         </li>  
                     </ul>
-                </div>                
-            </div>
-        </nav>       
-        <!-- Container -->
-        <div class="container bg-warning">
-            <!-- Carousel -->
-            <div id="demo" class="carousel slide" data-bs-ride="carousel">
-                <!-- Indicators/dots -->
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-                </div>
-                <!-- The slideshow/carousel -->
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src="img/carrusel-1.png" alt="Flow" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                    <img src="img/carrusel-2.png" alt="This is Urban" class="d-block w-100">
-                    </div>
-                    <div class="carousel-item">
-                    <img src="img/carrusel-3.png" alt="Drip" class="d-block w-100">
-                    </div>
-                </div>
-                <!-- Left and right controls/icons -->
-                <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon"></span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-                    <span class="carousel-control-next-icon"></span>
-                </button>
-            </div>
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
-                        <div class="card">
-                            <img class="card-img-top" src="img/producto-blazer-negro-premium.jpg" alt="Card image">
-                            <div class="card-body">
-                                <h4 class="card-title">Blazer Negro Premium</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
-                        <div class="card">
-                            <img class="card-img-top" src="img/producto-abrigo-trendy-peludo.jpg" alt="Card image">
-                            <div class="card-body">
-                                <h4 class="card-title">Abrigo Trendy Peludo</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
-                        <div class="card">
-                            <img class="card-img-top" src="img/producto-lentes-sol-street.jpg" alt="Card image">
-                            <div class="card-body">
-                                <h4 class="card-title">Lentes de Sol Trendy</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
-                        <div class="card">
-                            <img class="card-img-top" src="img/producto-set-deportivo-urbano.jpg" alt="Card image">
-                            <div class="card-body">
-                                <h4 class="card-title">Set Deportivo Urbano</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
-                        <div class="card">
-                            <img class="card-img-top" src="img/producto-set-streetwear-oscuro.jpg" alt="Card image">
-                            <div class="card-body">
-                                <h4 class="card-title">Set Streetwear Oscuro</h4>
-                                <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
-                            </div>
-                        </div>
-                    </div>                                                                                                                                                                                    
                 </div>
             </div>
+        </nav>
+        <!--container-->
+        <div class="container-fluid bg-dark text-white p-3">
+            Hola Servicios<br>
+            <a href="index.php">volver</a>
         </div>
-        <!-- Footer -->   
+        <!--footer-->
         <div class="container-fluid bg-dark">
             <div class="row">
                 <div class="col-4"></div>
-                <div class="col-4" style="color:white"><strong>MiEmpresa@2026</strong></div>
+                <div class="col-4" style="color:white"><strong>MiMarcaDeRopa@2026</strong></div>
                 <div class="col-4"></div>
             </div>
         </div>
-        <!-- Modal -->     
+        <!--modal-->
     </body>
 </html>
