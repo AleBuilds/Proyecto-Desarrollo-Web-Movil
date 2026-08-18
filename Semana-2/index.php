e03b464eef35fa6117a70da3f3880fa2aa59737a
Silsuerenller <silelusar@gmail.com>
2026-08-17 23:48:46 -0400

    versión final avance semana 2
    Updated navbar links and added Font Awesome icons. Modified footer and modal structure for better clarity.

--- Semana-2/index.php
+++ Semana-2/index.php
@@ -5,60 +5,97 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <!-- Navbar -->
+        <!--navbar-->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <div class="container-fluid"> 
-                <a class="navbar-brand" href="index.php"><i class="material-icons">star_border</i></a>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><i class="fa fa-star-half-o" style="font-size:48px;color:white"></i></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Marca de Ropa</a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="marcaderopa.php">Quienes Somos</a></li>
-                                <li><a class="dropdown-item" href="#">Nuestro Equipo</a></li>
-                                <li><a class="dropdown-item" href="#">Mision</a></li>
-                            </ul>
-                        </li>                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="productos.php">Productos</a>
+                            <a class="nav-link dropdown-toggle" href="marcaderopa.php" role="button" data-bs-toggle="dropdown">Prendas</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="marcaderopa.php">Poleras</a></li>
+                            <li><a class="dropdown-item" href="marcaderopa.php">Polerones</a></li>
+                            <li><a class="dropdown-item" href="marcaderopa.php">Pantalones</a></li>
+                            <li><a class="dropdown-item" href="marcaderopa.php">Buzos</a></li>
+                            <li><a class="dropdown-item" href="productos.php">Accesorios</a></li>
+                        </ul>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="servicios.php">Servicios</a>
+                            <a class="nav-link" href="productos.php">productos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contacto.php">Contacto</a>
-                        </li>                                                 
+                            <a class="nav-link" href="servicios.php">servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contacto.php">contacto</a>
+                        </li>
                     </ul>
-                </div>  
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button>               
+                </div>
+                        <li class="nav-item">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">Acceder</button> 
+                        </li> 
             </div>
-        </nav>       
-        <!-- Container -->
-        <div class="container-fluid bg-warning">
-            <a href="marcaderopa.php">Ir a Marca de Ropa</a><br>
-            <a href="productos.php">Ir a Productos</a><br>
-            <a href="servicios.php">Ir a Servicios</a><br>
-            <a href="contacto.php">Ir a Contacto</a><br>
+        </nav>
+        <!--container-->
+        <div class="container-fluid bg-dark text-white p-3">
+            <a href="marcaderopa.php">ir a marca ropa</a><br>
+            <a href="productos.php">ir a productos</a><br>
+            <a href="servicios.php">ir a servicios</a><br>
+            <a href="contacto.php">ir a contacto</a><br>
         </div>
-        <!-- Footer -->   
+<div class="container bg-dark text-white ">
+        <!-- Carousel -->
+        <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+            <!-- Indicators/dots -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+            </div>
+
+            <!-- The slideshow/carousel -->
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="img/chico_poleron_blanco.jpg" alt="chico_poleron_blanco" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="img/chica_afro.jpg" alt="chica_afro" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="img/chica _con_bandana.jpg" alt="chica _con_bandana" class="d-block w-100">
+                </div>
+            </div>
+
+            <!-- Left and right controls/icons -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </button>
+            </div>
+        </div>
+        <!--footer-->
         <div class="container-fluid bg-dark">
             <div class="row">
                 <div class="col-4"></div>
-                <div class="col-4" style="color:white"><strong>MiEmpresa@2026</strong></div>
+                <div class="col-4" style="color:white"><strong>MiMarcaDeRopa@2026</strong></div>
                 <div class="col-4"></div>
             </div>
         </div>
-        <!-- Modal -->     
-        <div class="modal fade" id="myModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
+        <!--modal-->
+            <div class="modal fade" id="myModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
                     <!-- Modal Header -->
                     <div class="modal-header">
                         <h4 class="modal-title">Autenticación</h4>
@@ -87,8 +124,9 @@
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
                     </div>
+                    </div>
                 </div>
+                
             </div>
-        </div>
     </body>
 </html>
