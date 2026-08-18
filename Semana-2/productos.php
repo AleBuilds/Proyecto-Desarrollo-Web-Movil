f227c76e7811d8e48c2822dc986306330332e2bd
Silsuerenller <silelusar@gmail.com>
2026-08-17 23:50:42 -0400

    Update productos.php

--- Semana-2/productos.php
+++ Semana-2/productos.php
@@ -1,46 +1,48 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <title>Primera pagina</title>
+        <title>primera pagina</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>        
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <!-- Navbar -->
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <div class="container-fluid"> 
-                <a class="navbar-brand" href="index.php"><i class="material-icons">star_border</i></a>
+                <a class="navbar-brand" href="index.php"><i class="fa fa-star-half-o" style="font-size:48px;color:white"></i></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="marcaderopa.php" role="button" data-bs-toggle="dropdown">Marca de Ropa</a>
+                            <a class="nav-link dropdown-toggle" href="marcaderopa.php" role="button" data-bs-toggle="dropdown">Prendas</a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Quienes Somos</a></li>
-                                <li><a class="dropdown-item" href="#">Nuestro Equipo</a></li>
-                                <li><a class="dropdown-item" href="#">Mision</a></li>
+                                <li><a class="dropdown-item" href="marcaderopa.php">Poleras</a></li>
+                                <li><a class="dropdown-item" href="marcaderopa.php">Polerones</a></li>
+                                <li><a class="dropdown-item" href="marcaderopa.php">Pantalones</a></li>
+                                <li><a class="dropdown-item" href="marcaderopa.php">Buzos</a></li>
+                                <li><a class="dropdown-item" href="productos.php">Accesorios</a></li>
                             </ul>
                         </li>                        
                         <li class="nav-item">
-                            <a class="nav-link" href="productos.php">Productos</a>
+                            <a class="nav-link" href="productos.php">productos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="servicios.php">Servicios</a>
+                            <a class="nav-link" href="servicios.php">servicios</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contacto.php">Contacto</a>
+                            <a class="nav-link" href="contacto.php">contacto</a>
                         </li>  
                     </ul>
                 </div>                
             </div>
         </nav>       
         <!-- Container -->
-        <div class="container bg-warning">
+        <div class="container bg-dark text-white">
             <!-- Carousel -->
             <div id="demo" class="carousel slide" data-bs-ride="carousel">
                 <!-- Indicators/dots -->
@@ -52,13 +54,13 @@
                 <!-- The slideshow/carousel -->
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                    <img src="img/carrusel-1.png" alt="Flow" class="d-block w-100">
+                    <img src="img/chico_poleron_blanco.jpg" alt="chico_poleron_blanco" class="d-block w-100">
                     </div>
                     <div class="carousel-item">
-                    <img src="img/carrusel-2.png" alt="This is Urban" class="d-block w-100">
+                    <img src="img/chica_afro.jpg" alt="chica_afro" class="d-block w-100">
                     </div>
                     <div class="carousel-item">
-                    <img src="img/carrusel-3.png" alt="Drip" class="d-block w-100">
+                    <img src="img/chica _con_bandana.jpg" alt="chica _con_bandana" class="d-block w-100">
                     </div>
                 </div>
                 <!-- Left and right controls/icons -->
@@ -73,62 +75,72 @@
                 <div class="row">
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
                         <div class="card">
-                            <img class="card-img-top" src="img/producto-blazer-negro-premium.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/mark-adriane-bO3S03I2Aw8-unsplash.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">Blazer Negro Premium</h4>
+                                <h4 class="card-title">Chaquetas</h4>
                                 <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
+                                <a href="#" class="btn btn-primary">See Profile</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
                         <div class="card">
-                            <img class="card-img-top" src="img/producto-abrigo-trendy-peludo.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/jc-gellidon-4TQJPykwpR0-unsplash.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">Abrigo Trendy Peludo</h4>
+                                <h4 class="card-title">Chaquetas</h4>
                                 <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
+                                <a href="#" class="btn btn-primary">See Profile</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
                         <div class="card">
-                            <img class="card-img-top" src="img/producto-lentes-sol-street.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/mike-von-YsiSAp3ccvk-unsplash.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">Lentes de Sol Trendy</h4>
+                                <h4 class="card-title">Chaquetas</h4>
                                 <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
+                                <a href="#" class="btn btn-primary">See Profile</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
                         <div class="card">
-                            <img class="card-img-top" src="img/producto-set-deportivo-urbano.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/mark-adriane-bO3S03I2Aw8-unsplash.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">Set Deportivo Urbano</h4>
+                                <h4 class="card-title">Chaquetas</h4>
                                 <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
+                                <a href="#" class="btn btn-primary">See Profile</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
                         <div class="card">
-                            <img class="card-img-top" src="img/producto-set-streetwear-oscuro.jpg" alt="Card image">
+                            <img class="card-img-top" src="img/jc-gellidon-4TQJPykwpR0-unsplash.jpg" alt="Card image">
                             <div class="card-body">
-                                <h4 class="card-title">Set Streetwear Oscuro</h4>
+                                <h4 class="card-title">Chaquetas</h4>
                                 <p class="card-text">Some example text.</p>
-                                <a href="#" class="btn btn-primary">Ver Producto</a>
+                                <a href="#" class="btn btn-primary">See Profile</a>
                             </div>
                         </div>
-                    </div>                                                                                                                                                                                    
-                </div>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 col-xxl-1">
+                        <div class="card">
+
+                            <img class="card-img-top" src="img/mike-von-YsiSAp3ccvk-unsplash.jpg" alt="Card image">
+                            <div class="card-body">
+                                <h4 class="card-title">Chaquetas</h4>
+                                <p class="card-text">Some example text.</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
         <!-- Footer -->   
         <div class="container-fluid bg-dark">
             <div class="row">
                 <div class="col-4"></div>
-                <div class="col-4" style="color:white"><strong>MiEmpresa@2026</strong></div>
+                <div class="col-4" style="color:white"><strong>MiMarcaDeRopa@2026</strong></div>
                 <div class="col-4"></div>
             </div>
         </div>
