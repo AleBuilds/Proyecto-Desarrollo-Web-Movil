264421e6c8f1c7c738c2a9f1446f1b0bb39c090f
Alejandro Narváez <anarvaezbracho@gmail.com>
2026-09-14 21:05:47 -0300

    Semana-6 Commit

---
+++ Semana-6/graphql/models/usuario.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const usuarioSchema = new mongoose.Schema({
+    nombre: String,
+    pass: String
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
